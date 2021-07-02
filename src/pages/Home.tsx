19ce6a91f4b14fdcc7ee5628d40611f219c923fc
mauricioholanda <mauriciofholanda@gmail.com>
2021-07-02 09:13:15 -0300

    feat: create home screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,25 +2,28 @@ import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 
+import "../styles/auth.scss";
+import { Button } from "../components/Button";
+
 export function Home() {
   return (
-    <div>
+    <div id="page-auth">
       <aside>
         <img src={illustrationImg} alt="illustrationImg" />
         <strong>Crie salas de Q&amp;a ao vivo</strong>
         <p>Tire suas duvidas da sua audiencia em tempo-real</p>
       </aside>
       <main>
-        <div>
+        <div className="main-content">
           <img src={logoImg} alt="logo" />
-          <button>
+          <button className="create-room">
             <img src={googleIconImg} alt="logo google" />
             Crie sua sala com Google
           </button>
-          <div>ou entre em uma sala</div>
+          <div className="separator">ou entre em uma sala</div>
           <form>
             <input type="text" placeholder="Digite o código da sala" />
-            <button type="submit">Entrar</button>
+            <Button type="submit">Entrar</Button>
           </form>
         </div>
       </main>
