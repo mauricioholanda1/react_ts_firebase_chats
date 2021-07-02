5a81a96410b9e55bf0a64b0c78c2347ab340cc02
mauricioholanda <mauriciofholanda@gmail.com>
2021-07-02 09:49:00 -0300

    feat: create NewRoom screnn

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,32 @@
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import googleIconImg from "../assets/images/google-icon.svg";
+
+import "../styles/auth.scss";
+import { Button } from "../components/Button";
+
+export function NewRoom() {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="illustrationImg" />
+        <strong>Crie salas de Q&amp;a ao vivo</strong>
+        <p>Tire suas duvidas da sua audiencia em tempo-real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="logo" />
+          <h2>Crie uma nova sala</h2>
+          <form>
+            <input type="text" placeholder="Nome da sala" />
+            <Button type="submit">Criar sala</Button>
+          </form>
+
+          <p>
+            Quer entrar em uma sala existente?<a href="#">clique aqui</a>
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
