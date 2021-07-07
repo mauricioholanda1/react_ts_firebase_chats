1ec20c686e03154a2cd48be97c439312594488cf
mauricioholanda <mauriciofholanda@gmail.com>
2021-07-07 10:17:39 -0300

    refactor: context user

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,13 +4,12 @@ import googleIconImg from "../assets/images/google-icon.svg";
 import { useHistory } from "react-router-dom";
 import "../styles/auth.scss";
 import { Button } from "../components/Button";
-import { useContext } from "react";
-import { AuthContext } from "../App";
+import { useAuth } from "../hooks/useAuth";
 
 export function Home() {
   const history = useHistory();
 
-  const { user, signInWithGoogle } = useContext(AuthContext);
+  const { user, signInWithGoogle } = useAuth();
 
   async function handleCreateRoom() {
     if (!user) {
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -3,11 +3,10 @@ import logoImg from "../assets/images/logo.svg";
 import { Link } from "react-router-dom";
 import "../styles/auth.scss";
 import { Button } from "../components/Button";
-import { useContext } from "react";
-import { AuthContext } from "../App";
+import { useAuth } from "../hooks/useAuth";
 
 export function NewRoom() {
-  const { user } = useContext(AuthContext);
+  const { user } = useAuth();
 
   return (
     <div id="page-auth">
