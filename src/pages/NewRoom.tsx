5363b96cdaeae332bde7fbd67e6d788aba792f6e
mauricioholanda <mauriciofholanda@gmail.com>
2021-07-07 10:00:48 -0300

    feat: context auth

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -3,8 +3,12 @@ import logoImg from "../assets/images/logo.svg";
 import { Link } from "react-router-dom";
 import "../styles/auth.scss";
 import { Button } from "../components/Button";
+import { useContext } from "react";
+import { AuthContext } from "../App";
 
 export function NewRoom() {
+  const { user } = useContext(AuthContext);
+
   return (
     <div id="page-auth">
       <aside>
@@ -15,6 +19,7 @@ export function NewRoom() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="logo" />
+          <h1>{user?.name}</h1>
           <h2>Crie uma nova sala</h2>
           <form>
             <input type="text" placeholder="Nome da sala" />
